1cd6f6b7da84f9f1ade654a80aba1fb5e63e54f6
texnic <kotya.karapetyan@gmail.com>
2021-05-13 12:44:48 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,6 +13,9 @@ function display_c () {
         led.plot(x - 1, y - 1)
     }
 }
+WSJoyStick.onKey(KEY.F, function () {
+    display_problem()
+})
 input.onButtonPressed(Button.A, function () {
     c += -1
     if (c < 0) {
@@ -24,15 +27,9 @@ input.onButtonPressed(Button.A, function () {
 function new_problem () {
     a = randint(1, 5)
     b = randint(1, 5)
-    basic.showString("" + (a))
-    basic.showString("+")
-    basic.showString("" + (b))
-    basic.showString("=")
-    c = 3
-    display_c()
 }
-WSJoyStick.onKey(KEY.E, function () {
-	
+WSJoyStick.onKey(KEY.D, function () {
+    check_answer()
 })
 input.onButtonPressed(Button.AB, function () {
     check_answer()
@@ -45,12 +42,22 @@ input.onButtonPressed(Button.B, function () {
     }
     display_c()
 })
+function display_problem () {
+    basic.showString("" + (a))
+    basic.showString("+")
+    basic.showString("" + (b))
+    basic.showString("=")
+    c = 3
+    display_c()
+}
 function check_answer () {
     if (a + b == c) {
         basic.showIcon(IconNames.Yes)
+        soundExpression.happy.play()
         new_problem()
     } else {
         basic.showIcon(IconNames.No)
+        soundExpression.sad.play()
         display_c()
     }
 }
@@ -63,6 +70,7 @@ let y = 0
 WSJoyStick.JoyStickInit()
 basic.clearScreen()
 new_problem()
+display_problem()
 basic.forever(function () {
 	
 })
